80870969ccff388d4a4bbbc72f4178205e134d64
Adarsh <adarshbaraily05@gmail.com>
2026-08-31 18:24:26 +0530

    Use of boolean data type in java

---
+++ Student.java
@@ -0,0 +1,16 @@
+public class Student {
+    public static void main() {
+
+        boolean isStudent = true;
+        
+        if(isStudent) {
+            System.out.println("Yes , he/she is a student");
+        }
+        else {
+            System.out.println("No , he/she is not a student");
+        }
+
+    }
+}
+    
+
