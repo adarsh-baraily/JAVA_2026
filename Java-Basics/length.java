364ff0645b16eddc5f0db76d228cc9d48b535be1
Adarsh <adarshbaraily05@gmail.com>
2026-09-15 22:34:31 +0530

    Added basic java program based upon length calculation of arrays

---
+++ Java-Basics/length.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class length {
+    public static void main() {
+
+        Scanner sc = new Scanner(System.in);
+        String fruits[] = {"apple", "banana", "pear", "mango", "guava"};
+        int numfruits = fruits.length;
+
+        System.out.println("Length of fruit array is :" + numfruits);
+
+        sc.close();
+
+    }
+}
+
